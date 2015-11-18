a4215effcd1d2dedd1f754e395732e046c604bfe
Adam Ferriss <aferriss@gmail.com>
2015-11-17 21:46:12 -0800

    saving all images as sheets

--- mnist.py
+++ mnist.py
@@ -4,32 +4,64 @@ from numpy import append, array, int8, uint8, zeros
 from pylab import *
 from numpy import *
 from PIL import Image
-
+import math
 # data from  http://yann.lecun.com/exdb/mnist/
 #sliced from here http://g.sweyla.com/blog/2012/mnist-numpy/
 
 def load_mnist(dataset="training", digits=np.arange(10), path='.'):
 	fname_img = os.path.join(path, 'train-images-idx3-ubyte')
+	fname_lbl = os.path.join(path, 'train-labels-idx1-ubyte')
+
+	flbl = open(fname_lbl, 'rb')
+	magic_nr, size = struct.unpack(">II", flbl.read(8))
+	lbl = pyarray("b", flbl.read())
+	flbl.close()
+
 
 	fimg = open(fname_img, 'rb')
 	magic_nr, size, rows, cols = struct.unpack(">IIII", fimg.read(16))
 	img = pyarray("B", fimg.read())
 	fimg.close()
 
-	ind = [ k for k in range(size) ]
+	#ind = [ k for k in range(size) ]
+	ind = [ k for k in range(size) if lbl[k] in digits ]
 	N = len(ind)
 
 
 	images = zeros((N, rows, cols), dtype=uint8)
+	labels = zeros((N, 1), dtype=int8)
 	for i in range(len(ind)):
 		images[i] = array(img[ind[i]*rows*cols : (ind[i]+1)*rows*cols ]).reshape((rows, cols))
+		labels[i] = lbl[ind[i]]
+	return images, labels
 
-	return images
+curdigit = 0
+for num in range(10):
+	images, labels = load_mnist('training', digits=[num])
 
-images = load_mnist('training', digits='0')
+	#for i in range(60000):
+	#	im = Image.fromarray(images[i])
+	#	im.save("sheets/img"+str(i)+".jpg")
+	largeImage = Image.new("L", (2500,2500))
+	y = 0
+	print images.shape
+	#for i in xrange(0,500,28):
+	#    for j in xrange(0,500,28):
+			#if( i != 0):
+			#	if ( (i * 28) % 1120  == 0 ):
+			#		y += 28
+			#if( (i*40+j) < len(images) ):
+			#im = Image.fromarray(images[i*28+j])
+			#largeImage.paste(im, (i,j))
 
-for i in range(60000):
-	im = Image.fromarray(images[i])
-	im.save("numbers/img"+str(i)+".jpg")
+	for i in range(len(images)):
+		y = int(i/sqrt(len(images)))
+		x = int(i%int(sqrt(len(images))))
+		#print x
+		im = Image.fromarray(images[i])
+		largeImage.paste(im, (x*28, y*28))
+
+	largeImage.show()
+	largeImage.save("sheets/"+str(num)+".jpg")
 
 
